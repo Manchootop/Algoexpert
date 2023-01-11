debf7dd6300b49d16557e6fe5c922d5cb7d36530
Mariyan Georgiev <mariqn5000@gmail.com>
2023-01-11 23:07:35 +0200

    solved problems

--- frontend-expert/react/demo/test-app/src/Profiles.js
+++ frontend-expert/react/demo/test-app/src/Profiles.js
@@ -1,7 +1,11 @@
 import WelcomeBanner from "./WelcomeBanner";
 import Course from "./Course";
+import { UserContext } from "./UserContext";
+import { useContext } from "react";
 
-export default function Profile({user}) {
+export default function Profile() {
+  const user = useContext(UserContext)
+
   return (
     <>
       <WelcomeBanner user={user} />

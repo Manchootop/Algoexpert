4754ba432cb6db14588a74471f3ec0d588876995
Manchootop <mariqn5000@gmail.com>
2025-05-06 18:48:15 +0300

    solved problem with strings

---
+++ algoexpert/strings/longest_palindrome_string.py
@@ -0,0 +1,39 @@
+# def longest_palindrome_string(string):
+#     current_longest = [0, 1]
+#     for i in range(1, len(string)):
+#         odd = get_longest_palindrome_from(string, i - 1, i + 1)
+#         even = get_longest_palindrome_from(string, i - 1, i)
+#         longest = max(odd, even, key=lambda x: x[1] - x[0])
+#         current_longest = max(longest, current_longest, key=lambda x: x[1] - x[0])
+#     return string[current_longest[0]:current_longest[1]]
+
+# def get_longest_palindrome_from(string, left_idx, right_idx):
+#     while left_idx >= 0 and right_idx < len(string):    
+#         if string[left_idx] != string[right_idx]:
+#             break
+#         left_idx -= 1
+#         right_idx += 1
+#     return [left_idx + 1, right_idx]
+
+
+# if __name__ == "__main__":
+#     print(longest_palindrome_string("abaxyzzyxf"))
+
+def get_longest_palindrome_string(string):
+    current_longest = [0, 1]
+    for i in range(1, len(string)):
+        odd = get_longest_palindrome_from(string, i - 1, i + 1) # odd length abcba
+        even = get_longest_palindrome_from(string, i - 1, i) # even length abba
+        longest = max(odd, even, key=lambda x: x[1] - x[0])
+        current_longest = max(longest, current_longest, key=lambda x: x[1] - x[0])
+    return string[current_longest[0]:current_longest[1]]
+
+def get_longest_palindrome_from(string, left_idx, right_idx):
+    while left_idx >= 0 and right_idx < len(string):
+        if string[left_idx] != string[right_idx]:
+            break
+        left_idx -= 1
+        right_idx += 1
+    return [left_idx + 1, right_idx]
+
+
